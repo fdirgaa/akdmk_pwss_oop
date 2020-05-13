ca6c3babcf80fd88dd88c598ae2c058e299e2315
Ferdi Dirgantara <ferdifordev@gmail.com>
2020-05-13 07:02:10 +0700

    Delete of CRUD

--- objects/product.php
+++ objects/product.php
@@ -111,9 +111,10 @@ class Product
         $this->category_id = $row['category_id'];
     }
 
-    function update(){
- 
-      $query = "UPDATE
+    public function update()
+    {
+
+        $query = "UPDATE
                   " . $this->table_name . "
               SET
                   name = :name,
@@ -122,29 +123,45 @@ class Product
                   category_id  = :category_id
               WHERE
                   id = :id";
-   
-      $stmt = $this->conn->prepare($query);
-   
-      // posted values
-      $this->name=htmlspecialchars(strip_tags($this->name));
-      $this->price=htmlspecialchars(strip_tags($this->price));
-      $this->description=htmlspecialchars(strip_tags($this->description));
-      $this->category_id=htmlspecialchars(strip_tags($this->category_id));
-      $this->id=htmlspecialchars(strip_tags($this->id));
-   
-      // bind parameters
-      $stmt->bindParam(':name', $this->name);
-      $stmt->bindParam(':price', $this->price);
-      $stmt->bindParam(':description', $this->description);
-      $stmt->bindParam(':category_id', $this->category_id);
-      $stmt->bindParam(':id', $this->id);
-   
-      // execute the query
-      if($stmt->execute()){
-          return true;
-      }
-   
-      return false;
-       
-  }
+
+        $stmt = $this->conn->prepare($query);
+
+        // posted values
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->price = htmlspecialchars(strip_tags($this->price));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // bind parameters
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':price', $this->price);
+        $stmt->bindParam(':description', $this->description);
+        $stmt->bindParam(':category_id', $this->category_id);
+        $stmt->bindParam(':id', $this->id);
+
+        // execute the query
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    // delete the product
+    public function delete()
+    {
+
+        $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->id);
+
+        if ($result = $stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
